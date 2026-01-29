de30d4796c183e92bb07c729c69fb06fa2d4eac2
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2026-01-29 12:20:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,18 +69,18 @@ else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
 
 # MongoDB information
-DATA_DATABASE_URL = environ.get('DATA_DATABASE_URL', "")
+DATA_DATABASE_URL = environ.get('DATA_DATABASE_URL', "mongodb+srv://Dilshan:<db_password>@moviecluster.obxil1i.mongodb.net/?appName=Moviecluster")
 if len(DATA_DATABASE_URL) == 0:
     logger.error('DATA_DATABASE_URL is missing, exiting now')
     exit()
-FILES_DATABASE_URL = environ.get('FILES_DATABASE_URL', "")
+FILES_DATABASE_URL = environ.get('FILES_DATABASE_URL', "mongodb+srv://Dilshan:<db_password>@moviecluster.obxil1i.mongodb.net/?appName=Moviecluster")
 if len(FILES_DATABASE_URL) == 0:
     logger.error('FILES_DATABASE_URL is missing, exiting now')
     exit()
 SECOND_FILES_DATABASE_URL = environ.get('SECOND_FILES_DATABASE_URL', "")
 if len(SECOND_FILES_DATABASE_URL) == 0:
     logger.info('SECOND_FILES_DATABASE_URL is empty')
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Moviecluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
